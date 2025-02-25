af5c21bfd5d5c3cbb9d40dda4aad78660d6cf212
Rofaida <rofaidasobhy123@gmail.com>
2025-02-25 20:30:13 +0200

    Splash screan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,6 @@ dependencies {
     annotationProcessor ("androidx.room:room-compiler:2.6.1")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("com.google.android.material:material:1.12.0")
+
+    implementation("com.airbnb.android:lottie:6.6.2")
 }
--- app/src/main/java/com/example/biteright/splash/view/SplashFragment.java
+++ app/src/main/java/com/example/biteright/splash/view/SplashFragment.java
@@ -1,5 +1,6 @@
 package com.example.biteright.splash.view;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,9 +8,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.biteright.R;
@@ -17,7 +21,11 @@ import com.example.biteright.R;
 
 public class SplashFragment extends Fragment {
 
-    private TextView txt_splash;
+
+    private MediaPlayer eating_sound;
+    private ImageView logo;
+    private TextView txt_slogan;
+    private com.airbnb.lottie.LottieAnimationView lottieAnimationView;
 
     public SplashFragment() {
         // Required empty public constructor
@@ -29,6 +37,10 @@ public class SplashFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
 
     }
 
@@ -43,16 +55,64 @@ public class SplashFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         initUI(view);
-        onClickListener(view);
+        startEatingSound();
+        convertLottieWithLogo();
+        navigateToNextScreen(view);
+
     }
 
     private void initUI(View view){
-        txt_splash = view.findViewById(R.id.txt_splash);
+        logo = view.findViewById(R.id.logo);
+        lottieAnimationView = view.findViewById(R.id.lottieAnimationView);
+        txt_slogan = view.findViewById(R.id.txt_slogan);
     }
-    private void onClickListener(View view){
-        txt_splash.setOnClickListener(
-                v -> Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_welcomeFragment)
-        );
+
+    private void startEatingSound(){
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                eating_sound = MediaPlayer.create(getContext(), R.raw.eatingcookiessound);
+                eating_sound.start();
+            }
+        },1000);
+
 
     }
+
+
+
+    private void navigateToNextScreen(View view){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_welcomeFragment);
+            }
+        },6000);
+    }
+
+    private void convertLottieWithLogo() {
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
+                fadeOut.setDuration(1000);
+                fadeOut.setFillAfter(true);
+                lottieAnimationView.startAnimation(fadeOut);
+                lottieAnimationView.setVisibility(View.GONE);
+
+
+                logo.setVisibility(View.VISIBLE);
+                txt_slogan.setVisibility(View.VISIBLE);
+                AlphaAnimation fadeIn = new AlphaAnimation(0, 1);
+                fadeIn.setDuration(1000);
+                fadeIn.setFillAfter(true);
+                logo.startAnimation(fadeIn);
+                txt_slogan.startAnimation(fadeIn);
+            }
+        },4000);
+    }
+
 }
